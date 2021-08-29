4136aa5e078daac5dcfcbaab742f3a8aada41b62
LucasAquino09/ <lucasaqs09@gmail.com>
2021-08-29 11:55:00 -0300

    Login/Partials Front

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async(req, res) =>{
-    res.send('login buniti');
+router.get('/', function(req, res){
+    res.render('login');
 })
 
 
